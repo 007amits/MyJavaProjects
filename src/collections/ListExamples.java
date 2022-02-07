1dfac0640718b8d2cf76c4dc234de28bb7991a63
007amits <amitshukla5502@gmail.com>
2022-02-07 22:57:52 +0530

    Added some changes in lIst

--- src/collections/ListExamples.java
+++ src/collections/ListExamples.java
@@ -7,36 +7,35 @@ import java.util.Vector;
 public class ListExamples {
 
   public static void main(String[] args) {
-    List<Float> arr1 = new ArrayList<>();
+    List<Object> arr1 = new ArrayList<>();
     arr1.add(23.5f);
     arr1.add(26.8f);
     arr1.add(21f);
+    arr1.add("String");
     System.out.println("List elements are : "+arr1);
     
     /* 
-     * List can not take different types of data Types. 
      * List can store duplicate elements.
      * List store elements in inserted order
      * */
     
   //------------------------------------------------------------------------------------//
     
-    Vector<String> vec = new Vector<String>();  
+    Vector<Object> vec = new Vector<>();  
     //Adding elements using add() method of List  
     vec.add("Tiger");  
     vec.add("Lion");  
-    vec.add("Dog");  
+    vec.add(2);  
     vec.add("Elephant");  
     //Adding elements using addElement() method of Vector  
     vec.addElement("Rat");  
-    vec.addElement("Cat");  
+    vec.addElement(5);  
     vec.addElement("Deer");
     
     System.out.println("Vector elements are : "+vec);
     
     /* 
      * Vector extends AbstractList
-     * Vector can take different types of data Types. 
      * Vector store elements in inserted order
      * */
 

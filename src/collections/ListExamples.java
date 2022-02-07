09721fbcfdb4861f8c28e14781e42687999c5806
007amits <amitshukla5502@gmail.com>
2022-02-07 21:54:51 +0530

    Added some changes

--- src/collections/ListExamples.java
+++ src/collections/ListExamples.java
@@ -2,7 +2,6 @@ package collections;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 import java.util.Vector;
 
 public class ListExamples {
@@ -35,28 +34,6 @@ public class ListExamples {
      * Vector extends AbstractList
      * Vector can take different types of data Types. 
      * Vector store elements in inserted order
-     * */
-    
-    //------------------------------------------------------------------------------------//
-    
-    Stack st = new Stack();
-    st.push("Java");
-    st.add(1);
-    st.push(55);
-    st.add(53);
-    st.push("Latest");
-    
-    System.out.println("Stack elements are : "+st);
-    System.out.println("st.peek() : "+st.peek());
-    System.out.println("st.pop() : "+st.pop());
-    System.out.println("Stack elements are : "+st);
-    
-    /* 
-     * Stack extends Vector
-     * Stack can take different types of data Types. 
-     * Stack store elements in inserted order
-     * Works on FILO - First in Last out/LIFO - Last in First Out
-     * Perform operations like - empty(), peek(), pop(), push() in addition to vector
      * */
 
   }
---
+++ src/collections/StackExamples.java
@@ -0,0 +1,30 @@
+package collections;
+
+import java.util.Stack;
+
+public class StackExamples {
+
+  public static void main(String[] args) {
+    Stack st = new Stack();
+    st.push("Java");
+    st.add(1);
+    st.push(55);
+    st.add(53);
+    st.push("Latest");
+    
+    System.out.println("Stack elements are : "+st);
+    System.out.println("st.peek() : "+st.peek());
+    System.out.println("st.pop() : "+st.pop());
+    System.out.println("Stack elements are : "+st);
+    
+    /* 
+     * Stack extends Vector
+     * Stack can take different types of data Types. 
+     * Stack store elements in inserted order
+     * Works on FILO - First in Last out/LIFO - Last in First Out
+     * Perform operations like - empty(), peek(), pop(), push() in addition to vector
+     * */
+
+  }
+
+}

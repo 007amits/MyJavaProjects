9f2970361a8bd4e20b55f8dd06cd5bf848932f94
007amits <amitshukla5502@gmail.com>
2023-01-07 19:29:56 +0530

    Added snippet to mask the email

---
+++ src/MaskEmailAddress.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MaskEmailAddress {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String email = sc.next();
+    
+    System.out.println("Masked email address = "+maskEmailAddress(email));
+    
+  }
+
+  private static String maskEmailAddress(String email) {
+    String arr[]= email.split("@");
+    String username = arr[0];
+    StringBuilder sb1 = new StringBuilder(username);
+    for(int i=3; i<sb1.length()-2; i++) {
+      sb1.setCharAt(i, '*');
+    }
+    sb1.setCharAt(sb1.length()-1, '*');
+    username = sb1.toString();
+    
+    String substring = arr[1];
+    String arr2[] = substring.split("\\.");
+    String domain = arr2[0];
+    StringBuilder sb2 = new StringBuilder(domain);
+    for(int i=2; i<sb2.length(); i++) {
+      sb2.setCharAt(i, '*');
+    }
+    domain = sb2.toString();
+    
+    String maskedEmail = username+"@"+domain+"."+arr2[1];
+    return maskedEmail;
+  }
+
+}
